81cf8bba18c1b68970446eea30953aff9b5d1edc
Pallavi <pallavishettigar03@gmail.com>
2024-01-10 22:12:44 +0530

    add

---
+++ noodles.java
@@ -0,0 +1,22 @@
+// Chef has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 minute to cook.
+// Chef's restaurant has X stoves and only 1 packet can be cooked in a single stove at any minute.
+// How many customers can Chef serve in 
+// Y minutes if each customer orders exactly 
+// 1 packet of noodles?
+// input = 3 7 , expected output = 27
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			
+		Scanner input = new Scanner(System.in);
+		System.out.print(input.nextInt()*input.nextInt());
+		
+
+	}
+}
