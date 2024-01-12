8a6d617e96d2d96cadde223982de32bf2297098a
Pallavi <pallavishettigar03@gmail.com>
2024-01-12 21:34:11 +0530

    add changes

---
+++ addtwonumbers.java
@@ -0,0 +1,27 @@
+// Chef and Chefina are playing with dice. In one turn, both of them roll their dice at once.
+// They consider a turn to be good if the sum of the numbers on their dice is greater than 6
+// Given that in a particular turn Chef and Chefina got 
+// X and Y on their respective dice, find whether the turn was good.
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-->0)
+		{
+    		int a = sc.nextInt();
+    		int b = sc.nextInt();
+    		// write your code here
+    		System.out.println(a+b);
+		}
+		
+	}
+}
