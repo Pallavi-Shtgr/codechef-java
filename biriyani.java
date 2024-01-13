7f4f778c42839d7f2e64fa6f95752c0b0423e767
Pallavi <pallavishettigar03@gmail.com>
2024-01-13 22:04:32 +0530

    add changes

---
+++ biriyani.java
@@ -0,0 +1,24 @@
+// According to a recent survey, Biryani is the most ordered food. Chef wants to learn how to make world-class Biryani from a MasterChef. Chef will be required to attend the MasterChef's classes for 
+// X weeks, and the cost of classes per week is 
+// Y coins. What is the total amount of money that Chef will have to pay?
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+Scanner sc=new Scanner(System.in);
+int t=sc.nextInt();
+while(t-->0)
+{
+int x=sc.nextInt();
+int y=sc.nextInt();
+    System.out.println(x*y);
+}
+	}
+}
