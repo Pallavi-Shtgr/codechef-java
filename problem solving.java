5211b3b54fdb21b15030b31e8dd41c5949b0a2c7
Pallavi <pallavishettigar03@gmail.com>
2024-02-06 22:50:00 +0530

    add new files via upload

---
+++ problem solving.java
@@ -0,0 +1,23 @@
+// CodeChef Learn Problem Solving
+// In the new CodeChef Learn module, under the "Learn Problem Solving" section, there are two courses for each language. For eg. "Python Beginner - Part 1" and "Python Beginner - Part 2". These courses help you get started with CodeChef contests.
+// Currently there are courses for 4 languages, and hence there are 8 courses in this section. But suppose there are courses for 
+// N languages, what will be the total number of courses in this section?
+// Input Format:
+
+// The only line of input will contain a single integer 
+// N, denoting the number of languages for which there are courses.
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print(input.nextInt()*2);
+
+	}
+}
