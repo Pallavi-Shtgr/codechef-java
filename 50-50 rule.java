1898d020a716fbd27dabccd280c0065199d91135
Pallavi <pallavishettigar03@gmail.com>
2024-03-16 19:47:59 +0530

    add changes

--- 50-50 rule.java
+++ 50-50 rule.java
@@ -1,6 +1,5 @@
 
-// Schools of Chefland use the 50−50 rule to grade their students. As per the rule , students are awarded :Z grade, if their attendance is strictly less than 
-// 50%- 50% .
+// Schools of Chefland use the 50−50 rule to grade their students. As per the rule , students are awarded :Z grade, if their attendance is strictly less than 50%- 50% .
 // F grade, if they score strictly less than 50% marks, given that their attendance is greater than or equal to 50% .
 // A grade, otherwise.
 // You are given two integers,X and Y, denoting the percentage of attendance and percentage of marks obtained by Alice. What grade will Alice get?
