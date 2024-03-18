b4bb1c670cdcdff4509aa6bf3457d341597f2903
Pallavi <pallavishettigar03@gmail.com>
2024-03-18 16:37:56 +0530

    add new changes

--- creatin string.java
+++ creatin string.java
@@ -2,3 +2,12 @@
 
 // Create a string named fruit and assign the value "Apple" to it.
 // Output fruit to the console.
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String fruit ="Apple";
+		System.out.print(fruit);
+	}
+}
