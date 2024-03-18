194670f3a65902e98ddd98c5bc68399754439af6
Pallavi <pallavishettigar03@gmail.com>
2024-03-18 17:12:47 +0530

    initial commit

--- strings and integer.java
+++ strings and integer.java
@@ -7,3 +7,16 @@
 // You want to output 2569
 // Try running the given code given in the IDE as it is - it will give a Compilation error
 // Fix the error so that both the variables become strings and the output is 2569
+
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		// your code goes here
+		String num1 = "25";
+        String num2 = "69";
+        System.out.print(num1 + num2);
+	}
+}
