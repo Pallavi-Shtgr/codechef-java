413c16e44ae14e159fc43f0b255eb09596e86010
Pallavi <pallavishettigar03@gmail.com>
2024-03-18 17:12:04 +0530

    add new changes

---
+++ strings and integer.java
@@ -0,0 +1,9 @@
+// Strings and Integers
+// Java uses the + sign for both addition and concatenation.
+// Numbers are added.
+// Strings are concatenated.
+// We cannot mix the two
+// You are given a program which does the following
+// You want to output 2569
+// Try running the given code given in the IDE as it is - it will give a Compilation error
+// Fix the error so that both the variables become strings and the output is 2569
