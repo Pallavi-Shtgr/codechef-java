092485734c7229c5e79b604c3091a0905cd92fe6
Pallavi <pallavishettigar03@gmail.com>
2024-03-18 17:35:30 +0530

    add changes

--- outputiing string.java
+++ outputiing string.java
@@ -3,3 +3,13 @@
 
 // Create a string variable word and assign the text "Programming" to it
 // Print the characters o and r from the string word in separate lines using the syntax defined above
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		String word = "Programming";
+        System.out.println(word.charAt(2));
+        System.out.print(word.charAt(1));
+	}
+}
