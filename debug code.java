f6cf688cbc06482051d9999f211ee1ccf4212270
Pallavi <pallavishettigar03@gmail.com>
2024-08-12 06:40:02 +0530

    Add changes

---
+++ debug code.java
@@ -0,0 +1,2 @@
+// You are given the age of a person as the variable age. Will this person be able to vote?
+// Output 1 or 0 assuming that the voting age is 18 i.e., a person's age has to be greater than or equal to 18 to vote.
