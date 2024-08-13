8c2e77b994744e3ec76cd5e8bfaa35fb0309a95c
Pallavi <pallavishettigar03@gmail.com>
2024-08-13 17:56:38 +0530

    add changes

---
+++ encoding.java
@@ -0,0 +1,2 @@
+// Chef recently graduated Computer Science in university, so he was looking for a job. He applied for several job offers, but he eventually settled for a software engineering job at ShareChat. Chef was very enthusiastic about his new job and the first mission assigned to him was to implement a message encoding feature to ensure the chat is private and secure.
+// Chef has a message, which is a string S with length N containing only lowercase English letters. It should be encoded in two steps as follows:
