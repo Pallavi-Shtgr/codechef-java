76d71e1d22ce3adfea78ecce2e4ce2da150d61b4
Pallavi <pallavishettigar03@gmail.com>
2024-08-15 07:33:36 +0530

    add chnages

---
+++ checf diet.java
@@ -0,0 +1,6 @@
+// Chef decided to go on a diet during the following N days (numbered 1 through N). Part of the diet plan is to eat K grams of protein during each day. For each valid i, Chef wants to buy 
+// A i grams of protein in the morning of the i-th day and then eat 
+// K grams of protein as part of his dinner. If he has any protein remaining, he can store it and use it in later dinners. Initially, Chef is storing 0 grams of protein.
+
+// Determine whether Chef will have enough protein all the time during his diet. In case he will not have enough, find the first day on which Chef will be unable to eat 
+// K grams of protein.
